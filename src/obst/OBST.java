2e7c5a676e3069c42e28d8a1d2f5c3111823e085
AmiR <amirkzm0098@gmail.com>
2021-06-01 10:45:18 +0430

    obst structure 1.0

---
+++ src/obst/OBST.java
@@ -0,0 +1,11 @@
+package obst;
+
+import java.util.ArrayList;
+
+public class OBST {
+    ArrayList<Word> words;
+
+    public OBST(ArrayList<Word> words) {
+        this.words = words;
+    }
+}
---
+++ src/obst/Word.java
@@ -0,0 +1,13 @@
+package obst;
+
+public class Word {
+    String word;
+    String translation;
+    double provability;
+
+    public Word(String word, String translation, double provability) {
+        this.word = word;
+        this.translation = translation;
+        this.provability = provability;
+    }
+}
