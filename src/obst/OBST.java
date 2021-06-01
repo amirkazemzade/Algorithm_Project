6534454401ab04e771f52a4eeec4fa43dbb9461b
AmiR <amirkzm0098@gmail.com>
2021-06-01 11:16:03 +0430

    obst 1.1

--- src/obst/OBST.java
+++ src/obst/OBST.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 public class OBST {
     ArrayList<Word> words;
+    int n, m;
+    double q;
 
     public OBST(ArrayList<Word> words) {
         this.words = words;
+        n = words.size();
+        m = n + 1;
+        double sumOfP = 0;
+        for (int i = 0; i < n; i++) {
+            sumOfP += words.get(i).provability;
+        }
+        q = (1 - sumOfP) / m;
     }
+
+
 }
+
+
