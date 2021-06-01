a4fd6660f13ef153ca33cfff8eae5bd7be413f60
amirkzm0098 <amirkzm0098@gmail.com>
2021-06-01 16:35:34 +0430

    obst 1.2

--- src/obst/OBST.java
+++ src/obst/OBST.java
@@ -7,6 +7,10 @@ public class OBST {
     int n, m;
     double q;
 
+    double[][] elements;
+    double[][] weight;
+    int[][] root;
+
     public OBST(ArrayList<Word> words) {
         this.words = words;
         n = words.size();
@@ -18,7 +22,35 @@ public class OBST {
         q = (1 - sumOfP) / m;
     }
 
+    public void makeTree() {
+        elements = new double[n + 2][n + 1];
+        weight = new double[n + 2][n + 1];
+        root = new int[n + 1][n + 1];
 
+        for (int i = 1; i <= n + 1; i++) {
+            elements[i][i - 1] = q;
+            weight[i][i - 1] = q;
+        }
+
+        for (int l = 1; l <= n; l++) {
+            for (int i = 1; i <= n - l + 1; i++) {
+                int j = i + l - 1;
+                weight[i][j] = weight[i][j - 1] + words.get(j).provability + q;
+                for (int r = i; r <= j; r++) {
+                    if (i == r) {
+                        elements[i][j] = elements[i][r - 1] + elements[r + 1][j] + weight[i][j];
+                        root[i][j] = r;
+                    } else {
+                        double temp = elements[i][r - 1] + elements[r + 1][j] + weight[i][j];
+                        if (temp < elements[i][j]) {
+                            elements[i][j] = temp;
+                            root[i][j] = r;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
