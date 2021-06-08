a849f479b5d7133e50127c272454ca95e2c5a560
amirAHS2000 <ahs.amirhosein78@gmail.com>
2021-06-08 20:34:47 +0430

    update translation layout and controller

--- src/ui/TranslationLayoutController.java
+++ src/ui/TranslationLayoutController.java
@@ -1,4 +1,36 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class TranslationLayoutController {
+    @FXML
+    private TextArea input_text;
+
+    @FXML
+    private TextArea output_text;
+
+    @FXML
+    private Button translate_button;
+
+    @FXML
+    private Button update_database_button;
+
+    @FXML
+    private Button tree_mode_button;
+
+    public void onTranslateClicked(ActionEvent event){
+
+    }
+
+    public void onUpdateDatabaseClicked(ActionEvent event){
+
+    }
+
+    public void onTreeModeClicked(ActionEvent event){
+
+    }
+
 }
