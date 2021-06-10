d26cba511ae376e59a2d5cedd5c1de47d8fdd500
amirkzm0098 <amirkzm0098@gmail.com>
2021-06-10 10:10:25 +0430

    ram usage applied to search

--- src/translator/WordTranslator.java
+++ src/translator/WordTranslator.java
@@ -1,5 +1,7 @@
 package translator;
 
+import ui.Main;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,32 +10,26 @@ import java.util.Scanner;
 public class WordTranslator {
     ArrayList<TreeNode> startTree;
     ArrayList<TreeNode> tree;
-    int nextDataIndex = 1;
-    File startData;
-    File currentData;
+    int nextDataIndex = 0;
     String dataPath;
+    boolean isStartTree = true;
 
 
     public WordTranslator() throws FileNotFoundException {
         dataPath = "src\\data\\data";
-        readStartTree();
-        tree = startTree;
-        currentData = startData;
+        tree = startTree = readTree();
     }
 
     public WordTranslator(char firstCase) throws FileNotFoundException {
         dataPath = "src\\dataByLetter\\data_" + firstCase;
-        readStartTree();
-        tree = startTree;
-        currentData = startData;
+        tree = startTree = readTree();
     }
 
     public String translate(String word) throws FileNotFoundException {
         int index = 1;
         int realIndex = 1;
-        nextDataIndex = 1;
+        nextDataIndex = Main.settings.getRamUsage() + 1;
         tree = startTree;
-        currentData = startData;
         while (true) {
             if (index >= tree.size()) {
                 index -= tree.size();
@@ -47,7 +43,6 @@ public class WordTranslator {
             TreeNode node = tree.get(index);
             if (node.getKey() == -1) {
                 tree = startTree;
-                currentData = startData;
                 return "?";
             } else {
                 int compare = word.compareTo(node.getWord());
@@ -65,32 +60,28 @@ public class WordTranslator {
     }
 
     private ArrayList<TreeNode> readTree() throws FileNotFoundException {
-        currentData = new File(dataPath + nextDataIndex + ".txt");
-        nextDataIndex++;
-
-        Scanner in = new Scanner(currentData);
-        ArrayList<TreeNode> tree = new ArrayList<>();
-
-        while (in.hasNext()) {
-            String line = in.nextLine();
-            String[] lineNode = line.split(" ");
-            tree.add(new TreeNode(Integer.parseInt(lineNode[0]), lineNode[1], lineNode[2]));
+        ArrayList<TreeNode> readTree = new ArrayList<>();
+        if (isStartTree) {
+            readTree.add(new TreeNode(-2, "start", "start"));
+            isStartTree = false;
         }
 
-        return tree;
-    }
+        for (int i = 0; i <= Main.settings.getRamUsage(); i++) {
+            File data = new File(dataPath + nextDataIndex + ".txt");
+            nextDataIndex++;
 
-    private void readStartTree() throws FileNotFoundException {
-        startTree = new ArrayList<>();
-        startTree.add(new TreeNode(-2, "start", "start"));
-        startData = new File(dataPath + "0.txt");
-        Scanner in = new Scanner(startData);
+            if (!data.exists()) break;
 
-        while (in.hasNext()) {
-            String line = in.nextLine();
-            String[] lineNode = line.split(" ");
-            startTree.add(new TreeNode(Integer.parseInt(lineNode[0]), lineNode[1], lineNode[2]));
+            Scanner in = new Scanner(data);
+
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                String[] lineNode = line.split(" ");
+                readTree.add(new TreeNode(Integer.parseInt(lineNode[0]), lineNode[1], lineNode[2]));
+            }
+            in.close();
         }
+        return readTree;
     }
 
 }
